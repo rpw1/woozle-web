48449b1d8fa2cf6e2175256c5c290b8add4fcccb
rpw1 <ryan.will@outlook.com>
2024-04-21 21:38:40 -0400

    more churn on ngrx. not sure what I need to keep or remove

--- src/app/game/services/progress-bar/progress-bar.service.ts
+++ src/app/game/services/progress-bar/progress-bar.service.ts
@@ -1,21 +1,19 @@
 import { Injectable, inject } from '@angular/core';
-import { PlayerService } from '../player/player.service';
-import { QueueService } from '../../../queue/services/queue.service';
 import { GuessService } from '../guess/guess.service';
-import { WoozleTaskType } from '../../../queue/models/woozle-task-type';
-import { WoozleTask } from '../../../queue/models/woozle-task';
-import { GuessType } from '../../models/guess-type';
-import { filter } from 'rxjs';
-import { GameConstants } from '../../models/game-constants';
-import { WoozleTaskState } from '../../../queue/models/woozle-task-state';
+import { PlayerService } from '../player/player.service';
+import { QueueState } from '../../../queue/state/queue-state.model';
+import { Store } from '@ngrx/store';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProgressBarService {
-
-  private guessService = inject(GuessService);
-  private playerService = inject(PlayerService);
-  private queueService = inject(QueueService);
+  
+  constructor(private store: Store<QueueState>,
+    private guessService: GuessService,
+    private playerService: PlayerService
+  ) {
+    
+  }
 
 }
--- src/app/queue/state/queue-state.selector.ts
+++ src/app/queue/state/queue-state.selector.ts
@@ -1,10 +1,6 @@
 import { createFeatureSelector, createSelector } from '@ngrx/store';
 import { QueueState } from './queue-state.model';
 import { deepClone } from 'fast-json-patch';
+import { WoozleTaskState } from '../models/woozle-task-state';
 
 export const getQueueState = createFeatureSelector<QueueState>('queueState');
-
-export const shiftQueue = createSelector(
-  getQueueState,
-  (state: QueueState) => (deepClone(state))
-)
