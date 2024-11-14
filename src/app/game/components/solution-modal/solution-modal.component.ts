c5f81ef2842f3429da1898d5d68d591aba2199f9
Ryan Williams <ryanw@knowledgeservices.com>
2024-11-14 09:49:39 -0500

    Correcting Solution Modal Logic

--- src/app/game/components/solution-modal/solution-modal.component.ts
+++ src/app/game/components/solution-modal/solution-modal.component.ts
@@ -1,34 +1,42 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  OnInit,
+} from '@angular/core';
 import { NgbActiveModal, NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { Store } from '@ngrx/store';
-import { filter } from 'rxjs';
-import { GameActions } from '../../state/actions/game.actions';
+import { filter, Observable, take } from 'rxjs';
+import { Track } from '../../../content/state/models/track';
 import { GameState } from '../../state/models/game-state.model';
 import { Game } from '../../state/models/game.model';
-import { selectCurrentGameState, selectSolution } from '../../state/selectors/game.selector';
+import {
+  selectCurrentGameState,
+  selectSolution,
+} from '../../state/selectors/game.selector';
 
 @Component({
   selector: 'app-solution-modal',
   standalone: true,
-  imports: [
-    CommonModule,
-    NgbModule
-  ],
+  imports: [CommonModule, NgbModule],
   templateUrl: './solution-modal.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SolutionModalComponent {
+export class SolutionModalComponent implements OnInit {
   private readonly activeModal = inject(NgbActiveModal);
   private readonly gameStore = inject(Store<Game>);
   readonly GameState = GameState;
-  readonly endingGameState$ = this.gameStore.select(selectCurrentGameState).pipe(
-    filter(x => x !== GameState.ACTIVE)
-  );
-  readonly solution$ = this.gameStore.select(selectSolution);
+  readonly endingGameState$ = this.gameStore
+    .select(selectCurrentGameState)
+    .pipe(filter((x) => x !== GameState.ACTIVE));
+  solution$: Observable<Track> | undefined;
+
+  ngOnInit(): void {
+    this.solution$ = this.gameStore.select(selectSolution).pipe(take(1));
+  }
 
   closeModal() {
-    this.gameStore.dispatch(GameActions.reset());
-    this.activeModal.close();
+    this.activeModal.close(true);
   }
 }
