a75a7372ddfe11232ca3061d45fc0b38a4bceb64
rpw1 <ryan.will@outlook.com>
2024-11-16 08:51:09 -0500

    removing lazy loading images

--- src/app/content/components/content/content.component.ts
+++ src/app/content/components/content/content.component.ts
@@ -1,15 +1,20 @@
-import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+} from '@angular/core';
 import { SpotifyContent } from '../../state/models/spotify-content';
-import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-content',
   standalone: true,
-  imports: [NgOptimizedImage],
+  imports: [],
   templateUrl: './content.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ContentComponent {
-  @Input({required: true}) content!: SpotifyContent;
+  @Input({ required: true }) content!: SpotifyContent;
   @Output() selectedContent = new EventEmitter<SpotifyContent>();
 }
