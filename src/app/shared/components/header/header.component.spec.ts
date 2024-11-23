3fca31f25a54b5d0ef7ce1740424d13d363110ed
rpw1 <ryan.will@outlook.com>
2024-11-22 21:14:46 -0500

    fixing unit tests

--- src/app/shared/components/header/header.component.spec.ts
+++ src/app/shared/components/header/header.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HeaderComponent } from './header.component';
+import { provideRouter, RouterModule } from '@angular/router';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
@@ -8,7 +9,12 @@ describe('HeaderComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HeaderComponent]
+      imports: [
+        HeaderComponent,
+      ],
+      providers: [
+        provideRouter([])
+      ]
     })
     .compileComponents();
 
