4a89fbb08ed3a27152f9153bab29f8fa98bf3892
rpw1 <ryan.will@outlook.com>
2024-11-22 23:56:35 -0500

    fixing test

--- src/app/devices/state/available-devices.service.spec.ts
+++ src/app/devices/state/available-devices.service.spec.ts
@@ -1,12 +1,18 @@
 import { TestBed } from '@angular/core/testing';
 
 import { AvailableDevicesService } from './available-devices.service';
+import { MockProvider } from 'ng-mocks';
+import { SpotifyService } from '../../shared/services/spotify.service';
 
 describe('AvailableDevicesService', () => {
   let service: AvailableDevicesService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        MockProvider(SpotifyService)
+      ]
+    });
     service = TestBed.inject(AvailableDevicesService);
   });
 
