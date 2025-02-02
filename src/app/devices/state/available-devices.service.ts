1e26d0e75bcb07669a732aa3e18d1e102de69733
rpw1 <ryan.will@outlook.com>
2025-02-02 13:53:06 -0500

    adding back player hoping I don't have more caching issues with pwa

--- src/app/devices/state/available-devices.service.ts
+++ src/app/devices/state/available-devices.service.ts
@@ -1,11 +1,12 @@
 import { inject, Injectable } from '@angular/core';
-import { exhaustMap, firstValueFrom, ReplaySubject } from 'rxjs';
+import { exhaustMap, ReplaySubject } from 'rxjs';
 import { SpotifyService } from '../../shared/services/spotify.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AvailableDevicesService {
+  private player: any | undefined;
   private readonly spotifyService = inject(SpotifyService);
 
   private readonly spotifyPlaybackDevice = new ReplaySubject<void>(1);
@@ -14,9 +15,7 @@ export class AvailableDevicesService {
   )
 
   async loadAvailableDevices(): Promise<void> {
-    const availableDevices = await firstValueFrom(this.spotifyService.getAvailableDevices());
-    const activePlayer = availableDevices.devices.find(x => x.name === 'Woozle Built-In Spotify Web Player')
-    if (!activePlayer) {
+    if (!this.player) {
       const accessToken = localStorage.getItem('access_token') ?? '';
       await this.initPlaybackSDK(accessToken, 0.5);
     }
@@ -25,22 +24,22 @@ export class AvailableDevicesService {
   }
 
   // This is needed for the music to play on iOS devices
-  // Not removing this file because I can't test on mobile until I push my changes
   setPlayerActiveElement() {
-    // if (this.spotifyPlayer) {
-    //   this.spotifyPlayer.activateElement();
-    // }
+    if (this.player) {
+      this.player.activateElement();
+    }
   }
 
   private async initPlaybackSDK(token: string, volume: number) {
     const { Player } = await this.waitForSpotifyWebPlaybackSDKToLoad();
     const player = new Player({
-      name: 'Woozle Built-In Spotify Web Player',
+      name: `Woozle Built-In Spotify Web Player - ${(window as any).navigator.userAgentData?.platform ?? 'UNKNOWN'}`,
       getOAuthToken: (cb: any) => {
         cb(token);
       },
       volume
     });
+    this.player = player;
 
     player.addListener('initialization_error', ({ message }: { message: string }) => {
       console.error(message);
@@ -58,8 +57,10 @@ export class AvailableDevicesService {
       console.error(message);
     });
 
-    player.addListener('ready', ({ device_id }: { device_id: string }) => {
+    player.addListener('ready', async ({ device_id }: { device_id: string }) => {
       console.log('[Angular Spotify] Ready with Device ID', device_id);
+      await new Promise(resolve => setTimeout(resolve, 500));
+      // Ensuring the device is registered in spotify before calling their API again.
       this.spotifyPlaybackDevice.next();
     });
 
