01901d2e6879990b624f9ca8b21696d8b286911b
rpw1 <ryan.will@outlook.com>
2025-06-28 12:19:26 -0400

    fixing all things with upgrade

--- woozle-web/karma.conf.js
+++ woozle-web/karma.conf.js
@@ -12,7 +12,6 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),  
       require('karma-coverage'),
-      
       require('karma-junit-reporter')
     ],
     client: {
--- woozle-web/src/test.ts
+++ woozle-web/src/test.ts
@@ -1,19 +1,15 @@
 // This file is required by karma.conf.js and loads recursively all the .spec and framework files
 
-import 'zone.js/testing';
 import { getTestBed } from '@angular/core/testing';
-import {
-  BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting
-} from '@angular/platform-browser-dynamic/testing';
+import { BrowserTestingModule, platformBrowserTesting } from '@angular/platform-browser/testing';
 import { ngMocks } from 'ng-mocks';
 
 ngMocks.autoSpy('jasmine');
 
 // First, initialize the Angular testing environment.
 getTestBed().initTestEnvironment(
-  BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting(), {
+  BrowserTestingModule,
+  platformBrowserTesting(), {
     teardown: { destroyAfterEach: false }
 }
 );
