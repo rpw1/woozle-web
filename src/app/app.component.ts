58ee9902f1eb96a8540c7b471e3ff7edfd21c058
rpw1 <ryan.will@outlook.com>
2025-09-21 10:25:20 -0400

    removing tacky loading spinner

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,10 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/components/header/header.component';
 import { ToastComponent } from "./shared/components/toast/toast.component";
-import { LoadingSpinnerComponent } from './shared/components/loading-spinner/loading-spinner.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, ToastComponent, LoadingSpinnerComponent],
+  imports: [RouterOutlet, HeaderComponent, ToastComponent],
   templateUrl: './app.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
